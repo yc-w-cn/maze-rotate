5bf5bc7534700a0525b07e9e406206d42c6062b7
YC.W <contact@wangyuchen.cn>
2023-11-18 23:59:51 +0800

    chore: support gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "gh-pages" ? "/maze-rotate/" : "/",
   plugins: [react()],
-})
+}));
